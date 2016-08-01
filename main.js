6b5962ac4795e6fe9cbbe3a9156afd8abb494f41
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-08-01 10:09:39 -0600

    added Michales way

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// My way
+
 var firstArray = ['cat', 'dog', 'fish', 'zebra'];
 var secondArray = ['lion', 'aardvark', 'gorilla'];
 
@@ -7,6 +9,20 @@ function combineArray(first, second) {
 }
 
 console.log('New Array ' , combineArray(firstArray, secondArray));
+
+console.log('First Array ' , firstArray);
+console.log('Second Array ' , secondArray);
+
+// Michaels way
+
+function combineAndSort(firstArray, secondArray) {
+  var newArray = firstArray.slice();
+  secondArray.forEach(function(el) {
+    newArray.push(el)
+  })
+  return newArray.sort();
+}
+console.log('New Array ' , combineAndSort(firstArray, secondArray));
 
 console.log('First Array ' , firstArray);
 console.log('Second Array ' , secondArray);
